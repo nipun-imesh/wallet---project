c4045bb60c22af356f736740ccf7377ac4b31400
nipun imesh <imeshnipun@gmail.com>
2026-02-02 18:15:29 +0530

    i

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,6 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
-      "expo-router/babel",
       // MUST be last
       "react-native-reanimated/plugin",
     ],
